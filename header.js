b2a257e15fee663c6047a80c40632616300f53bd
Jose Francisco Terraza <jterraza@est.frba.utn.edu.ar>
2023-01-31 19:15:53 -0300

    Agregamos el login todavia falta enlazarlo

--- header.js
+++ header.js
@@ -1,3 +1,5 @@
+$('#scroll-up').css('visibility', 'hidden');
+
 // funcionalidad que permite que el header se haga grande o diminuto
 $(document).ready(function(){
 
@@ -10,3 +12,30 @@ $(document).ready(function(){
 	});
 
 });
+
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 1500 ){
+			$('#scroll-up').css('visibility', 'visible');;
+		} else {
+			$('#scroll-up').css('visibility', 'hidden');
+		}
+	});
+
+});
+
+// funcionalidad de desplazar hacia arriba
+const scrollUp = document.querySelector("#scroll-up");
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
+  //$('#scroll-up').css('visibility', 'hidden');
+  //$('#scroll-up').css('visibility', 'visible');
